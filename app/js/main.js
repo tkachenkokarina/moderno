db14a197c68b54307667ebc558adb9c01d4ea976
vulpecula-kt <dyukovakarina@gmail.com>
2019-12-29 19:08:34 +0200

    added product-one and page and page-404 and about

--- app/js/main.js
+++ app/js/main.js
@@ -42,6 +42,17 @@ $('.menu__btn').on('click', function(){
 });
 
 
+$('.product-one__tabs .tab').on('click', function(event) {
+    var id = $(this).attr('data-id');
+        $('.product-one__tabs').find('.tab-item').removeClass('active-tab').hide();
+        $('.product-one__tabs .tabs').find('.tab').removeClass('active');
+        $(this).addClass('active');
+        $('#'+id).addClass('active-tab').fadeIn();
+        return false;
+    });
+
+
+
 $('.header__btn-menu').on('click', function(){
     $('.header__box').toggleClass('active');
 
